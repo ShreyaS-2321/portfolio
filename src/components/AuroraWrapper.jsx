63fd7de4c47ab8561dd783b0bed8e674305ea9ef
Shreya Shrivastava <147529420+ShreyaS-2321@users.noreply.github.com>
2025-07-13 19:54:39 +0530

    Update AuroraWrapper.jsx

--- src/components/AuroraWrapper.jsx
+++ src/components/AuroraWrapper.jsx
@@ -2,7 +2,7 @@ import Aurora from './Aurora';
 
 export default function AuroraWrapper() {
   return (
-    <div className="relative h-[16vh] w-full overflow-hidden">
+    <div className="relative h-[12vh] w-full overflow-hidden">
       <Aurora />
     </div>
   );
