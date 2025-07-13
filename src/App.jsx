841997702c9410985f18f64cfab6fe5d3786a0c4
Shreya Shrivastava <147529420+ShreyaS-2321@users.noreply.github.com>
2025-07-13 19:54:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import AuroraWrapper from './components/AuroraWrapper';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
